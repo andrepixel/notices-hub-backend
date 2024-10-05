bdc6c60a38ab36215cda299b3e494822afcc7d8e
André <devandrecontact@gmail.com>
2024-10-05 15:21:12 -0300

    initial commit!

---
+++ src/main/java/com/andrepixel/notices_hub_backend/NoticesHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andrepixel.notices_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticesHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticesHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrepixel/notices_hub_backend/NoticesHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrepixel.notices_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticesHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
